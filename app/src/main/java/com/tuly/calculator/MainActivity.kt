a8a00def7a2d4417de2e95bef97a1a2369b81cda
Tulitu Vlad <tulituvlad@gmail.com>
2022-12-24 17:01:40 +0200

    Updated

--- app/src/main/java/com/tuly/calculator/MainActivity.kt
+++ app/src/main/java/com/tuly/calculator/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import com.tuly.calculator.presentation.CalculatorScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                CalculatorScreen()
+                //
             }
         }
     }
