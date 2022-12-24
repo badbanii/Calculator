6afa3415e82aeb05166284671897c41d0bba399a
Tulitu Vlad <tulituvlad@gmail.com>
2022-12-24 14:58:16 +0200

    -Migrated Material2 to Material3 -Added CalculatorUiAction.kt -Added CalculatorUiActions.kt

--- app/src/main/java/com/tuly/calculator/MainActivity.kt
+++ app/src/main/java/com/tuly/calculator/MainActivity.kt
@@ -10,37 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tuly.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MaterialTheme {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
     }
 }
